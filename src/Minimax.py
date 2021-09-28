8560eb89bcee2f9f91ea0f1e160b424fe84bdbb9
clee2023 <clee2@wpi.edu>
2021-09-28 00:21:26 -0400

    basic evaluate board function
    not sure if the values are good in terms of weight and such

--- src/Minimax.py
+++ src/Minimax.py
@@ -1,4 +1,15 @@
 class Minimax:
+
+    """The additional value we give to certain important locations"""
+    score_values = [3, -2, 2, 2, 2, 2, -2, 3,
+                    -2, -2, -1, -1, -1, -1, -2, -2,
+                    2, -1, 2, 2, 2, 2, -1, 2,
+                    2, -1, 2, 0, 0, 2, -1, 2,
+                    2, -1, 2, 0, 0, 2, -1, 2,
+                    2, -1, 2, 2, 2, 2, -1, 2,
+                    -2, -2, -1, -1, -1, -1, -2, -2,
+                    3, -2, 2, 2, 2, 2, -2, 3]
+
     def __init__(self):
         self.alpha = 0
         self.beta = 0
@@ -10,5 +21,14 @@ class Minimax:
         pass
 
     def evaluate_board(self, board):
-        """Calculates the score of the given board state"""
-        pass
+        """Calculates the move score of the given board state"""
+        score = board.raw_score()
+        for i in range(64):  # iterate through each cell
+            addition = 0  # what will be added to score
+            val = board.boardstate[i]  # value of current cell
+            if val != 0:  # skips everything if cell value is 0
+                addition = self.score_values[i]
+                if val < 0:  # swaps value of addition if the side is -1
+                    addition *= -1
+            score += addition
+        return score
