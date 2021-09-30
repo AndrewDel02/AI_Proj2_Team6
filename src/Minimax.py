0abf349ae59189caa3935827d34cf087a3d4e71b
AndrewDel02 <andrewdel82@gmail.com>
2021-09-29 22:38:23 -0400

    Added documentation to Minimax

--- src/Minimax.py
+++ src/Minimax.py
@@ -28,9 +28,10 @@ class Minimax:
         best_move = Move(-1, our_color)
         depth = 1
         best_val = 0
-        self.time_out = time.time() + time_limit - 0.01
+        self.time_out = time.time() + time_limit - 0.01  # set time out time
+        # Iterative deepening, keep searching deeper until out of time error is thrown
         try:
-            if our_color == -1:
+            if our_color == -1:  # White players want to minimize their score, Black players want to maximize it
                 while True:
                     val, move = self.get_min_value(board, depth, -99999999, 99999999)
                     best_move = move
@@ -78,15 +79,18 @@ class Minimax:
         # print("minimizing at depth: " + str(depth))
         worst = 9999999
         move_to_make = Move(-1, -1)
+        # check for timeout
         if time.time() >= self.time_out:
             raise TimeRanOutException
 
+        # if this is an end node, evaluate it and return that
         if depth == 0:
             self.nodes_evaled += 1
             score = self.evaluate_board(board)
             # print("result is: " + str(score))
             return score, move_to_make
 
+        # otherwise find the lowest score of all the nodes in next level down
         for move in board.get_legal_moves():
             new_board = board.update(move)
             move_score, next_move = self.get_max_value(new_board, depth - 1, alpha, beta)
@@ -94,10 +98,9 @@ class Minimax:
                 worst = move_score
                 move_to_make = move
                 beta = min(beta, move_score)
-            if worst <= alpha:
+            if worst <= alpha:  # Alpha Beta pruning happens here
                 return worst, move_to_make
 
-        # print("result is: " + str(worst) + " at " + move_to_make.convert_location_to_ref_representation())
         return worst, move_to_make
 
     def get_max_value(self, board: Board, depth: int, alpha: float, beta: float):
@@ -106,15 +109,18 @@ class Minimax:
         # print("maximizing at depth: " + str(depth))
         best = -9999999
         move_to_make = Move(-1, 1)
+        # check for timeout
         if time.time() >= self.time_out:
             raise TimeRanOutException
 
+        # if this is an end node, evaluate it and return that
         if depth == 0:
             self.nodes_evaled += 1
             score = self.evaluate_board(board)
             # print("result is: " + str(score))
             return score, move_to_make
 
+        # otherwise find the highest score of all the nodes in next level down
         for move in board.get_legal_moves():
             new_board = board.update(move)
             move_score, next_move = self.get_min_value(new_board, depth - 1, alpha, beta)
